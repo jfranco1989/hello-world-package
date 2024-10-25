6b445b73d343f26c287ab5a329aa145051bffab5
Jean Franco <jfranco1989@gmail.com>
2024-10-25 10:56:54 -0300

    Empacotando projeto simples

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
+# setup.py
+
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
-    page_description = f.read()
-
-with open("requiremnts.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
-    name="image-processing",
-    version="0.0.1",
+    name="hello-world-package",
+    version="0.1",
+    description="A simple greeting package",
     author="Jean Franco",
     author_email="jfranco1989@gmail.com",
-    description="Pacote para processar comparações entre imagens",
-    long_description=page_description,
+    license="MIT",
     long_description_content_type="text/markdown",
-    url="https://github.com/jfranco1989/image-processing-package.git",
+    url="https://github.com/jfranco1989/hello-world-package.git",
     packages=find_packages(),
-    install_requires=requirements,
-    python_requires='>=3.8',
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
